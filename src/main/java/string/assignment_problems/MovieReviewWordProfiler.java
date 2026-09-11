8d63f86b0d4410ca3cea80c5df7e188c3c099a9f
shreyash0107-web <shreyashkumarjee@gmail.com>
2026-09-11 21:55:11 +0530

    Add Movie Review Word Profiler

---
+++ src/main/java/string/assignment_problems/MovieReviewWordProfiler.java
@@ -0,0 +1,32 @@
+package string.assignment_problems;
+import java.util.Scanner;
+public class MovieReviewWordProfiler {
+    static void classifyWordLengths(String review) {
+        String[] words = review.split("\\s+");
+        int shortWords = 0;
+        int mediumWords = 0;
+        int longWords = 0;
+        for (int i = 0; i < words.length; i++) {
+            int length = words[i].length();
+            if (length >= 1 && length <= 4) {
+                shortWords++;
+
+            } else if (length <= 8) {
+                mediumWords++;
+
+            } else {
+                longWords++;
+            }
+        }
+        System.out.println("Short: " + shortWords);
+        System.out.println("Medium: " + mediumWords);
+        System.out.println("Long: " + longWords);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter movie review: ");
+        String review = sc.nextLine();
+        classifyWordLengths(review);
+        sc.close();
+    }
+}
