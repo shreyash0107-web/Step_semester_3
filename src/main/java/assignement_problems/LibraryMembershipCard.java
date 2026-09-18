d26f90fd671a62a25720e7fb5cb13c659b76189f
shreyash0107-web <shreyashkumarjee@gmail.com>
2026-09-18 23:53:22 +0530

    Add Library Membership Card

---
+++ src/main/java/assignement_problems/LibraryMembershipCard.java
@@ -0,0 +1,33 @@
+class MembershipCard {
+    static String libraryName;
+    static String validUntil;
+    String studentName;
+    static {
+        libraryName = "SRM Central Library";
+        validUntil = "May 2027";
+        System.out.println("Library info loaded");
+    }
+    MembershipCard(String studentName) {
+        this.studentName = studentName;
+    }
+    void display() {
+        System.out.println(
+            "Membership card issued: " + studentName
+        );
+    }
+}
+public class LibraryMembershipCard {
+    public static void main(String[] args) {
+        String[] names = {
+            "Ananya",
+            "Rohan",
+            "Priya",
+            "Arjun",
+            "Sneha"
+        };
+        for (int i = 0; i < names.length; i++) {
+            MembershipCard card = new MembershipCard(names[i]);
+            card.display();
+        }
+    }
+}
