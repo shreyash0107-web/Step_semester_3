aa3bbf61834e3aa70415caf275e540ea7c4f2868
shreyash0107-web <shreyashkumarjee@gmail.com>
2026-09-18 23:13:15 +0530

    Add Employee Constructor

---
+++ src/main/java/string/assignment_problems/EmployeeConstructor.java
@@ -0,0 +1,32 @@
+class EmployeeConstructor {
+    String empId;
+    String empName;
+    double salary;
+    boolean isIntern;
+    public EmployeeConstructor(String empId, String empName, double salary) {
+        this.empId = empId;
+        this.empName = empName;
+        this.salary = salary;
+        this.isIntern = false;
+    }
+    public EmployeeConstructor(String empId, String empName) {
+        this(empId, empName, 0);
+        this.isIntern = true;
+    }
+    public void printProfile() {
+        System.out.println(
+            empId + " | " +
+            empName + " | Rs " +
+            salary + " | Intern: " +
+            isIntern
+        );
+    }
+    public static void main(String[] args) {
+        EmployeeConstructor permanent =
+            new EmployeeConstructor("E-101", "Divya", 65000);
+        EmployeeConstructor intern =
+            new EmployeeConstructor("E-102", "Arjun");
+        permanent.printProfile();
+        intern.printProfile();
+    }
+}
