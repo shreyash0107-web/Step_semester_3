1a9376dce6f6a9e3dac29703fd16bf5a467746b0
shreyash0107-web <shreyashkumarjee@gmail.com>
2026-09-24 23:27:17 +0530

    Add Student Discount and Fine Ledger

---
+++ src/main/java/class_problems/Discount_fine.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+class LibraryMember {
+    protected String memberId;
+    protected int borrowLimit;
+    private int[] fineHistory = new int[10];
+    private int fineCount = 0;
+    public LibraryMember(String memberId,int borrowLimit) {
+        this.memberId = memberId;
+        this.borrowLimit = borrowLimit;
+    }
+    protected void chargeFine(int amount) {
+        if (fineCount < fineHistory.length) {
+            fineHistory[fineCount++] = amount;
+        }
+    }
+    public int[] getFineHistory() {
+        return Arrays.copyOf(
+            fineHistory,
+            fineCount
+        );
+    }
+    public int getTotalFine() {
+        int total = 0;
+        for (int i = 0; i < fineCount; i++) {
+            total += fineHistory[i];
+        }
+        return total;
+    }
+}
+class StudentMember extends LibraryMember {
+    private String course;
+    public StudentMember( String memberId,int borrowLimit,String course) {
+        super(memberId, borrowLimit);
+        this.course = course;
+    }
+    @Override
+    protected void chargeFine(int amount) {
+        super.chargeFine(amount / 2);
+    }
+}
+public class Discount_fine {
+    public static void main(String[] args) {
+        StudentMember s =
+            new StudentMember(
+                "STU5",
+                3,
+                "CSE"
+            );
+        s.chargeFine(100);
+        System.out.println(
+            s.getTotalFine()
+        );
+        int[] history =s.getFineHistory();
+        history[0] = 999;
+        System.out.println(
+            Arrays.toString(
+                s.getFineHistory()
+            )
+        );
+    }
+}
